e3b09cc6a5a62c64bb3308a842a6c9800ffed054
Shokhzod Murodov <m_axwel_l@Shokhzods-MacBook-Pro.local>
2022-05-05 10:47:42 +0500

    description changed

--- src/views/product/ItemDescription.vue
+++ src/views/product/ItemDescription.vue
@@ -1,15 +1,20 @@
 <template>
   <header-product>
     <section class="section-container mt-3 p-4">
-      <div class=" mt-4" :key="'characteristics_item_' + main.id" v-for="main in product.characteristics">
-        <h6>{{ main.header }}</h6>
-        <table class="dotted-table text-dark">
-          <tr v-for="(items, index) in main.values" :key="'char_'  + index">
-            <th><span>{{ items.key }}</span></th>
-            <th><span>{{ items.value }}</span></th>
-          </tr>
-        </table>
-      </div>
+      <b-row>
+        <b-col cols="12" class="col-xl-5 col-lg-6 col-md-12 col-sm-12">
+          <div class=" mt-4" :key="'characteristics_item_' + main.id" v-for="main in product.characteristics">
+            <h6>{{ main.header }}</h6>
+            <table class="dotted-table text-dark">
+              <tr v-for="(items, index) in main.values" :key="'char_'  + index">
+                <th><span>{{ items.key }}</span></th>
+                <th><span>{{ items.value }}</span></th>
+              </tr>
+            </table>
+          </div>
+        </b-col>
+      </b-row>
+
     </section>
   </header-product>
 
