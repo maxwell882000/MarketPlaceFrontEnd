59ae0de33a66445d44cb482a2d4f2062a65859d8
m_axwel_l <shaxa882@gmail.com>
2022-11-20 21:25:44 +0500

    fixing

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import store from "../../store/index";
 
 // const SERVER = "http://185.217.131.41:49153";
-const SERVER = process.env.NODE_ENV === 'production' ? "http://185.217.131.41" : "http://127.0.0.1:8000";
+const SERVER = process.env.NODE_ENV === 'production' ? "http://0.0.0.0" : "http://127.0.0.1:8000";
 const instance = axios.create({
     baseURL: SERVER + "/api/",
     headers: {
